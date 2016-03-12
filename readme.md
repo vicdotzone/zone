60d15055dc7340ffb942abb92a92d9eec94725d2
Neal Granger <neal@metalab.co>
2016-03-12 14:12:19 -0800

    bootstarting reactive app

--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 ### WELCOME TO VIC.ZONE
+
+```bash
+nvm install
+npm install
+npm start
+open http://localhost:8080/
+```
---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  function () {
+    return (
+      <div>
+        VIC.ZONE
+      </div>
+    );
+  }
+};
