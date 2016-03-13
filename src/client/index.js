2522702a4c51623061e84adb8d291f234f0d438e
Neal Granger <neal@metalab.co>
2016-03-12 20:09:41 -0800

    fixing the lints

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import injectTapEventPlugin from 'react-tap-event-plugin';
-//Needed for onTouchTap
-//Can go away when react 1.0 release
-//https://github.com/zilverline/react-tap-event-plugin
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin();
 
 import React from 'react';
@@ -10,5 +10,5 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 // Render the main component into the dom
-var rootEl = document.getElementById('app');
+const rootEl = document.getElementById('app');
 ReactDOM.render(<App />, rootEl);
